4a1361a3f37411e5bc13a95df29dc0ea86c47b73
AfonsoFernando051 <afonsofernando051@gmail.com>
2023-10-31 23:34:24 -0300

    criar componente de reserva

--- src/app/login-layout/dashboard/dashboard-componentes/confirmation-dialog/confirmation-dialog.component.ts
+++ src/app/login-layout/dashboard/dashboard-componentes/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { ConfirmationDialogService } from './confirmation-dialog-service/confirmation-dialog.service';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-confirmation-dialog',
@@ -9,10 +10,12 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class ConfirmationDialogComponent {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, public dialogRef: MatDialogRef<ConfirmationDialogComponent>) {}
   
   reservar(data: any){
     console.log(data);
   }
-
+  sair(){
+    this.dialogRef.close();
+  }
 }
